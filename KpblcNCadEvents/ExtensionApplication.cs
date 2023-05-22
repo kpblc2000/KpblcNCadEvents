b24a4cdcd1623d9c3e827b93fb155bde50abc1e6
kpblc2000 <kpblc2000@gmail.com>
2023-05-22 17:30:37 +0300

    Start

---
+++ KpblcNCadEvents/ExtensionApplication.cs
@@ -0,0 +1,58 @@
+﻿using HostMgd.ApplicationServices;
+using KpblcNCadEvents.Infrastructure;
+using Teigha.Runtime;
+
+namespace KpblcNCadEvents
+{
+    public class ExtensionApplication : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            Application.DocumentManager.DocumentActivated += OnDocumentActivated;
+            // Application.DocumentManager.DocumentActivationChanged += OnDocumentActivationChanged; // Не реализовано в NC23
+            Application.DocumentManager.DocumentCreateStarted += OnDocumentStarted;
+            Application.DocumentManager.DocumentBecameCurrent += OnDocumentBecameCurrent;
+            Application.DocumentManager.DocumentCreated += OnDocumentCreated;
+            Application.DocumentManager.DocumentDestroyed += OnDocumentDestroyed;
+        }
+
+        private void OnDocumentDestroyed(object sender, DocumentDestroyedEventArgs e)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            LogService.LogMessage(doc == null ? null : doc.Name);
+        }
+
+        private void OnDocumentCreated(object sender, DocumentCollectionEventArgs e)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            LogService.LogMessage(doc == null ? null : doc.Name);
+        }
+
+        private void OnDocumentBecameCurrent(object sender, DocumentCollectionEventArgs e)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            LogService.LogMessage(doc == null ? null : doc.Name);
+        }
+
+        private void OnDocumentStarted(object sender, DocumentCollectionEventArgs e)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            LogService.LogMessage(doc == null ? null : doc.Name);
+        }
+
+        private void OnDocumentActivationChanged(object sender, DocumentActivationChangedEventArgs e)
+        {
+           Document doc = Application.DocumentManager.MdiActiveDocument;
+            LogService.LogMessage(doc == null ? null : doc.Name);
+        }
+
+        private void OnDocumentActivated(object sender, DocumentCollectionEventArgs e)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            LogService.LogMessage(doc == null ? null : doc.Name);
+        }
+
+        public void Terminate() { }
+        
+    }
+}
---
+++ KpblcNCadEvents/Infrastructure/LogService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace KpblcNCadEvents.Infrastructure
+{
+    internal static class LogService
+    {
+        public static void LogMessage(string DocumentName, [CallerMemberName] string Message = null)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(_logFileName)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(_logFileName));
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(_logFileName, true))
+                {
+                    writer.WriteLine(DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss.fff")
+                        + DocumentName == null ? "Без имени документа" :
+                        DocumentName
+                        + "\t:\t"
+                        + Message == null ? "Нет сообщения" : Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private static string _logFileName =
+            Path.Combine(Environment.GetEnvironmentVariable("appdata"), @"KpblcNCadEvents\KpblcNCadEvents.Log");
+    }
+}
